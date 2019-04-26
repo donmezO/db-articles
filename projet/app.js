8b246219620c1f5a9e4c66d11d92166056c811ea
Oguz <donmez.oguz1@gmail.com>
2019-04-26 14:20:06 +0200

    Fetch

--- projet/app.js
+++ projet/app.js
@@ -1,4 +1,53 @@
 // XHR
-let xhr = new XMLHttpRequest();
-	xhr.open('GET','https://my-json-server.typicode.com/donmezO/db-articles');
+/*let xhr = new XMLHttpRequest();
+	//xhr.open('GET','https://my-json-server.typicode.com/donmezO/db-articles/articles');
+	xhr.open('GET', 'article.html.tpl');
+	xhr.onload = function() {
+		// template article.html
+		let parserHTML = new DOMParser();
+		let template = parserHTML.parseFromString(xhr.response, 'text/html')
+		console.log(xhr.response);
+
+
+		let xhrJSON = new XMLHttpRequest();
+			xhrJSON.open('GET','https://my-json-server.typicode.com/donmezO/db-articles/articles');
+			xhrJSON.onload = function() {
+				// articles JSON
+				let articles;
+				try{
+					articles = JSON.parse(xhrJSON.response);
+				} catch(e){}
+				
+				if (articles) {
+					for (let article of articles){
+						document.querySelector('#articles').appendChild(template.body.first);
+					}
+				}
+			}
+			xhrJSON.send();
+	}
 	xhr.send();
+	*/
+
+// FETCH
+
+fetch('article.html.tpl')
+	.then( response => response.text() )
+	.then( templateString => {
+		let parserHTML = new DOMParser();
+		let template   = parserHTML.parseFromString(templateString,'text/html').body.firstChild;
+
+		console.log(template);
+		fetch('https://my-json-server.typicode.com/donmezO/db-articles/articles')
+			.then( response => response.json() )
+			.then( articles => {
+				if (articles){
+					console.log(articles)
+					for (let article of articles){
+						let clone = template.cloneNode(true);
+							clone.querySelector('[article-content]').innerText = article.content;
+						document.querySelector(#articles).appendChild(clone);
+					}
+				}
+			});
+	});
